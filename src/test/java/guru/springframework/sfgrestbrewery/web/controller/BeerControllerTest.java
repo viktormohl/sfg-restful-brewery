30f04f610445fa0e3bfddd4bb4554549f4171f60
viktormohl <viktor.mohl@gmail.com>
2021-06-08 23:13:55 +0200

    add WebFluxTest

---
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,57 @@
+package guru.springframework.sfgrestbrewery.web.controller;
+
+import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
+import guru.springframework.sfgrestbrewery.services.BeerService;
+import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import org.assertj.core.api.BDDAssumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+@WebFluxTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerDto validBeer;
+
+    @BeforeEach
+    void setUp() {
+        validBeer = BeerDto.builder()
+                .beerName("Test beer")
+                .beerStyle("PALE_ALE")
+                .upc(BeerLoader.BEER_1_UPC)
+                .build();
+    }
+
+    @Test
+    void getBeerById() {
+        UUID beerId = UUID.randomUUID();
+        given(beerService.getById(eq(beerId), any())).willReturn(validBeer);
+
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/api/v1/beer/{beerId}").build(beerId))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(BeerDto::getBeerName, equalTo(validBeer.getBeerName()));
+
+    }
+}
